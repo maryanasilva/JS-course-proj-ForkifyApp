85ef8fa1086f61bd56a293e5fc864ef190e4e2fc
Mariana Silva <maryana_silva@sapo.pt>
2024-01-25 18:32:37 +0000

    all done

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "4fc619bc-c30d-4fe2-86ed-a31333bdbb5c";
+export const MODAL_CLOSE_SEC = 2.5;
